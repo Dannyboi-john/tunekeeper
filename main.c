48079bec5531cc4b9bb6a9ee7c08f10f9d75e15e
danny prince <danny@debian0.local>
2026-02-26 21:41:46 -0330

    Added button with clicker counter - still expanding skills

--- main.c
+++ main.c
@@ -1,18 +1,53 @@
 #include "lvgl/lvgl.h"
 #include <unistd.h>
+#include <stdio.h>
+
+static int click_count = 0;
+static lv_obj_t * my_label1;
+
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        click_count++;
+        fprintf(stderr, "Event Fired\n");
+        
+        lv_label_set_text_fmt(my_label1,
+            "Clicked %d times",
+            click_count);
+    }
+}
+
 
 int main(void)
 {
     /* Initialize LVGL */
     lv_init();
+    printf("Program Started!\n");
+    fflush(stdout);
 
     /* Initialize SDL display */
     lv_display_t * disp = lv_sdl_window_create(800, 480);
 
-    /* Create a label */
-    lv_obj_t * label = lv_label_create(lv_screen_active());
-    lv_label_set_text(label, "This is the humble beginning of a beatiful thing <3");
-    lv_obj_center(label);
+    
+    /* Initialize mouse and keyboard */
+    lv_sdl_mouse_create();
+    lv_sdl_keyboard_create();
+
+    /* Trying to create a button */
+    lv_obj_t * my_button1 = lv_button_create(lv_screen_active());
+    /* Set parent-sized witdth, and content-sized height */
+    lv_obj_set_size(my_button1, lv_pct(30), LV_SIZE_CONTENT);
+    /* Align to the right center with 30px offset horizontally */
+    lv_obj_align(my_button1, LV_ALIGN_CENTER, 0, 0);
+    /* Attaching the callback function inside main() */
+    lv_obj_add_event_cb(my_button1, btn_event_cb, LV_EVENT_CLICKED, NULL);
+
+    /* Create a label for the button */
+    my_label1 = lv_label_create(my_button1);
+    lv_label_set_text_fmt(my_label1, "Clicked 0 times");
+    lv_obj_set_style_text_color(my_label1, lv_color_hex(0xff0000), 0);
 
     while(1) {
         lv_timer_handler();
