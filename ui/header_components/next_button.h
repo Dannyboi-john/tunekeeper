9948bfe0bebe0e05cfe4a7f13707f411237ef5f5
Danny Prince <ddjjpp340@gmail.com>
2026-03-15 22:14:31 -0230

    Added prev and next button files

---
+++ ui/header_components/next_button.h
@@ -0,0 +1,8 @@
+#ifndef NEXT_BUTTON_H
+#define NEXT_BUTTON_H
+
+#include "lvgl/lvgl.h"
+
+
+
+#endif
---
+++ ui/header_components/prev_button.h
@@ -0,0 +1,8 @@
+#ifndef PREV_BUTTON_H
+#define PREV_BUTTON_H
+
+#include "lvgl/lvgl.h"
+
+
+
+#endif
